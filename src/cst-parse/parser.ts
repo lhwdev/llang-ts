bc177a4504e6911cc38da94765e9579c24180387
이현우 <lhwdev6@outlook.com>
2025-03-02 07:05:13 +0000

    Optimized parser function: unwrap unnecessary nested calls. - unwrap node() call and other calls; reduced nested function calls from 4(impl -> node -> lambda -> fn) to 2(impl -> fn)

--- src/cst-parse/parser.ts
+++ src/cst-parse/parser.ts
@@ -2,10 +2,16 @@ import { yellow } from "../utils/ansi.ts";
 import type { CstNode } from "../cst/CstNode.ts";
 import type { CstNodeInfo } from "../cst/CstNodeInfo.ts";
 import { node, nullableNode } from "./inlineNode.ts";
-import { type CstParseContext, withContext } from "./CstParseContext.ts";
+import { type CstParseContext, getContext, withContext } from "./CstParseContext.ts";
 import { ParseError } from "./parseError.ts";
 
+export const CstParserSymbol = Symbol("CstParserSymbol");
+
 abstract class CstParserClass<Node extends CstNode, Fn extends (...args: any[]) => any> {
+  get [CstParserSymbol](): true {
+    return true;
+  }
+
   abstract info: CstNodeInfo<Node>;
 
   abstract invoke(...args: Parameters<Fn>): ReturnType<Fn>;
@@ -51,20 +57,20 @@ function parserName(option: string | undefined, info: CstNodeInfo<any>): string 
   return yellow(option ?? extractName());
 }
 
-export function rawParser<Params extends any[], Node extends CstNode, R>(
-  info: CstNodeInfo<Node>,
-  surround: (result: () => R) => R,
-  raw: (...args: Params) => R,
-  impl: (...args: Params) => R = (...args: Params): R => surround(() => raw(...args)),
-): CstParser<Node, ((...args: Params) => R)> {
-  const parser = new class extends CstParser<Node, ((...args: Params) => R)> {
-    override info = info;
-    override invoke = impl;
-    override invokeRaw = raw;
-    override surround = surround;
-  }();
-  return Object.setPrototypeOf(Object.assign(impl, parser), Object.getPrototypeOf(parser));
-}
+// function rawParser<Params extends any[], Node extends CstNode, R>(
+//   info: CstNodeInfo<Node>,
+//   surround: (result: () => R) => R,
+//   raw: (...args: Params) => R,
+//   impl: (...args: Params) => R = (...args: Params): R => surround(() => raw(...args)),
+// ): CstParser<Node, ((...args: Params) => R)> {
+//   const parser = new class extends CstParser<Node, ((...args: Params) => R)> {
+//     override info = info;
+//     override invoke = impl;
+//     override invokeRaw = raw;
+//     override surround = surround;
+//   }();
+//   return Object.setPrototypeOf(Object.assign(impl, parser), Object.getPrototypeOf(parser));
+// }
 
 export function parser<Params extends any[], Node extends CstNode>(
   info: CstNodeInfo<Node>,
@@ -72,12 +78,27 @@ export function parser<Params extends any[], Node extends CstNode>(
   options?: { name?: string },
 ): CstParser<Node, ((...args: Params) => Node)> {
   Object.defineProperty(impl, "name", { value: parserName(options?.name, info) });
-  return rawParser(
-    info,
-    (inner) => node(info, inner),
-    impl,
-    (...args) => node(info, () => impl(...args)),
-  );
+  const invoke = (...args: Params): Node => {
+    const context = getContext();
+    const child = context.beginChild(info);
+    const skip = child.skipping();
+    if (skip) return skip;
+    try {
+      const node = context === child ? impl(...args) : withContext(child, () => impl(...args));
+      return child.end(node);
+    } catch (e) {
+      const result = child.endWithError(e);
+      if (!result) throw e;
+      return result;
+    }
+  };
+  const parser = new class extends CstParser<Node, ((...args: Params) => Node)> {
+    override info = info;
+    override invoke = invoke;
+    override invokeRaw = impl;
+    override surround = (inner: () => Node) => node(info, inner);
+  }();
+  return Object.setPrototypeOf(Object.assign(invoke, parser), Object.getPrototypeOf(parser));
 }
 
 export function nullableParser<Params extends any[], Node extends CstNode>(
@@ -86,10 +107,30 @@ export function nullableParser<Params extends any[], Node extends CstNode>(
   options?: { name?: string },
 ): CstParser<Node, ((...args: Params) => Node | null)> {
   Object.defineProperty(impl, "name", { value: parserName(options?.name, info) });
-  return rawParser(
-    info,
-    (inner) => nullableNode(info, inner),
-    impl,
-    (...args) => nullableNode(info, () => impl(...args)),
-  );
+  const invoke = (...args: Params): Node | null => {
+    const context = getContext();
+    const child = context.beginChild(info);
+    const skip = child.skipping();
+    if (skip) return skip;
+    child.hintType("nullable");
+    try {
+      const node = context === child ? impl(...args) : withContext(child, () => impl(...args));
+      if (node) {
+        return child.end(node);
+      } else {
+        return child.endWithError(null);
+      }
+    } catch (e) {
+      const result = child.endWithError(e);
+      if (!result) throw e;
+      return result;
+    }
+  };
+  const parser = new class extends CstParser<Node, ((...args: Params) => Node | null)> {
+    override info = info;
+    override invoke = invoke;
+    override invokeRaw = impl;
+    override surround = (inner: () => Node | null) => nullableNode(info, inner);
+  }();
+  return Object.setPrototypeOf(Object.assign(invoke, parser), Object.getPrototypeOf(parser));
 }
